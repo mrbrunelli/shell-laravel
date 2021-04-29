7d65a32e508e8e8275b81d59985f9150c4ddeb90
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-29 20:25:19 -0300

    add mkdir src

--- run_container.sh
+++ run_container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir src
+
 docker container run -d --name containerlaravel -p 8085:80 -v ${PWD}/src:/var/www/html imagemlaravel
 
 docker exec -u 1000 containerlaravel bash -c "composer create-project --prefer-dist laravel/laravel ."
